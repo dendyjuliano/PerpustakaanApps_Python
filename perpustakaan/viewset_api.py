56e89cee524fa06bf453e4302e20c3e3457057fb
Dendy <dendyjuliano2019@gmail.com>
2020-11-27 18:34:37 +0700

    Python RestAPI

---
+++ perpustakaan/viewset_api.py
@@ -0,0 +1,9 @@
+from perpustakaan.models import Kelompok
+from perpustakaan.serializers import KelompokSerializer
+from rest_framework import viewsets
+from rest_framework import permissions
+
+class KelompokViewSet(viewsets.ModelViewSet):
+    queryset = Kelompok.objects.all()
+    serializer_class = KelompokSerializer
+    permission_classes = [permissions.IsAuthenticated]
